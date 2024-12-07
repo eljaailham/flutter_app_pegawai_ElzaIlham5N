1cbb9a179b1801e40287b0c2dea0f8016420d16a
unknown <elzailhamadipratama@gmail.com>
2024-12-08 06:43:44 +0800

    elza_2210010091

--- lib/pages/profil.dart
+++ lib/pages/profil.dart
@@ -10,10 +10,34 @@ class Profil extends StatefulWidget {
 class _ProfilState extends State<Profil> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('ini profil'),
+  return Scaffold(
+    body: Center( 
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          Image.network(
+            'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnptem1pZHBpNjd6c21wZGFjejNyMDVxenltYzUwa20zN2VuMXpqaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/IpKxfPy33hMRy/200.webp',
+            width: 200,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 20), 
+          Text(
+            'NPM : 2210010091',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            'NAMA : ELZA ILHAM ADI PRATAMA',
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            'KELAS : 5N REGULER PAGI BANJARMASIN',
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
+
 }
